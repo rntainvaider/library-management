5bdcdfd0fbf29bfb5fb7c4b14439453c3e0ed7b3
Евгений Вовк <evgenijvovk@MacBook-Pro-Evgenij.local>
2025-05-22 22:02:46 +0300

    created the fastapi app

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def hello():
+    return "Hello world"
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
